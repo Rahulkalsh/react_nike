6dc4a1d885b8b21e0322c5b938479c6a4e898432
Rahulkalsh <108459884+Rahulkalsh@users.noreply.github.com>
2024-12-10 18:03:39 +0530

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,7 +7,6 @@ const Navigation = () =>{
 
             <ul>
                 <li href = "#">Menu</li>
-                <li href = "#">Location</li>
                 <li href = "#">About</li>
                 <li href = "#">Contact</li>
             </ul>
